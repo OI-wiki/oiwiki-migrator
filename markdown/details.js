4070c5a0a5bc80cc9c8bbb01d3b1e5abc4ed0235
mgt <mgt@oi-wiki.org>
2021-10-25 15:22:37 +0800

    chore: remove lodash

--- markdown/details.js
+++ markdown/details.js
@@ -1,7 +1,15 @@
 import { visit } from 'unist-util-visit'
-import lodash from 'lodash'
-const { trim } = lodash
-export default function attacher () {
+function trim (str, ch) {
+  let start = 0
+  let end = str.length
+
+  while (start < end && str[start] === ch) { ++start }
+
+  while (end > start && str[end - 1] === ch) { --end }
+
+  return (start > 0 || end < str.length) ? str.substring(start, end) : str
+}
+export default function attacher() {
   const processor = this
   return function transformer (ast) {
     visit(ast, 'detailsContainerSummary', function (node) {
