91cdce2eba9a75f9d7f3864092d151cfac1fa955
Xiaodai Dai <diauweb@live.com>
2021-10-25 15:00:28 +0000

    fix: use replace instead of replaceAll

--- markdown/details.js
+++ markdown/details.js
@@ -1,20 +1,21 @@
 import { visit } from 'unist-util-visit'
+
 function trim (str, ch) {
   let start = 0
   let end = str.length
 
   while (start < end && str[start] === ch) { ++start }
-
   while (end > start && str[end - 1] === ch) { --end }
 
   return (start > 0 || end < str.length) ? str.substring(start, end) : str
 }
-export default function attacher() {
+
+export default function attacher () {
   const processor = this
   return function transformer (ast) {
     visit(ast, 'detailsContainerSummary', function (node) {
       const md = processor.stringify({ type: 'paragraph', children: node.children })
-      const removeMduiMd = md.replaceAll(/mdui-shadow-\d+\s+/g, '')
+      const removeMduiMd = md.replace(/mdui-shadow-\d+\s+/g, '')
       const removeNewlineMd = trim(trim(removeMduiMd, ' '), '\n')
       const newMd = trim(removeNewlineMd, '"')
       node.children = processor.parse(newMd).children
