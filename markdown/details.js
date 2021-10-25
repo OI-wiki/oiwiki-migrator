86bd7e43f6b5265db66889fd331e51768c67d068
mgt <mgt@oi-wiki.org>
2021-10-25 11:26:53 +0800

    fix: mdui related bugs

--- markdown/details.js
+++ markdown/details.js
@@ -1,13 +1,14 @@
 import { visit } from 'unist-util-visit'
 import lodash from 'lodash'
-const { trim, trimEnd } = lodash
+const { trim } = lodash
 export default function attacher () {
   const processor = this
   return function transformer (ast) {
     visit(ast, 'detailsContainerSummary', function (node) {
       const md = processor.stringify({ type: 'paragraph', children: node.children })
-      const removeNewlineMd = trimEnd(trim(md, ' '), '\n')
-      const newMd = trim(removeNewlineMd, '"').replaceAll(/mdui-shadow-\d+/g, '').trim(' ')
+      const removeMduiMd = md.replaceAll(/mdui-shadow-\d+\s+/g, '')
+      const removeNewlineMd = trim(trim(removeMduiMd, ' '), '\n')
+      const newMd = trim(removeNewlineMd, '"')
       node.children = processor.parse(newMd).children
     })
   }
