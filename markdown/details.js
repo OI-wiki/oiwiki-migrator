6b0046091780dfdf6f49dad14b233419faff415d
mgt <mgt@oi-wiki.org>
2021-10-25 11:12:19 +0800

    use lodash trim

--- markdown/details.js
+++ markdown/details.js
@@ -1,20 +1,13 @@
 import { visit } from 'unist-util-visit'
-function trim (str, ch) {
-  let start = 0
-  let end = str.length
-
-  while (start < end && str[start] === ch) { ++start }
-
-  while (end > start && str[end - 1] === ch) { --end }
-
-  return (start > 0 || end < str.length) ? str.substring(start, end) : str
-}
+import lodash from 'lodash'
+const { trim, trimEnd } = lodash
 export default function attacher () {
   const processor = this
   return function transformer (ast) {
     visit(ast, 'detailsContainerSummary', function (node) {
       const md = processor.stringify({ type: 'paragraph', children: node.children })
-      const newMd = trim(md, '"').replaceAll(/mdui-shadow-\d+/g, '').trim(' ')
+      const removeNewlineMd = trimEnd(trim(md, ' '), '\n')
+      const newMd = trim(removeNewlineMd, '"').replaceAll(/mdui-shadow-\d+/g, '').trim(' ')
       node.children = processor.parse(newMd).children
     })
   }
