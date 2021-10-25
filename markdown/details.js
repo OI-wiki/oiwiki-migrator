43d8b658c6e79f65dcc05692428a3eac852f9cae
mgt <mgt@oi-wiki.org>
2021-10-25 10:29:16 +0800

    fix: trim mdui and quotes

--- markdown/details.js
+++ markdown/details.js
@@ -1,25 +1,21 @@
 import { visit } from 'unist-util-visit'
+function trim (str, ch) {
+  let start = 0
+  let end = str.length
 
+  while (start < end && str[start] === ch) { ++start }
+
+  while (end > start && str[end - 1] === ch) { --end }
+
+  return (start > 0 || end < str.length) ? str.substring(start, end) : str
+}
 export default function attacher () {
   const processor = this
-  const spliter = /[^\s"]+|"([^"]*)"/g
-
   return function transformer (ast) {
     visit(ast, 'detailsContainerSummary', function (node) {
       const md = processor.stringify({ type: 'paragraph', children: node.children })
-
-      // https://stackoverflow.com/a/18647776
-      let arr = []
-      let match
-      do {
-        match = spliter.exec(md)
-        if (match != null) {
-          arr.push(match[1] ? match[1] : match[0])
-        }
-      } while (match !== null)
-
-      arr = arr.filter(e => !e.startsWith('mdui-shadow'))
-      node.children = processor.parse(arr.join(' '))
+      const newMd = trim(md, '"').replaceAll(/mdui-shadow-\d+/g, '').trim(' ')
+      node.children = processor.parse(newMd).children
     })
   }
 }
